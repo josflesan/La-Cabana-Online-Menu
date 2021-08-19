b0ec5b4118971b0c5de31016862ddf1a651add45
Joflesan <s2088605@ed.ac.uk>
2021-08-19 14:15:24 +0100

    Added Description Styling

--- public/script/infoModal.js
+++ public/script/infoModal.js
@@ -16,13 +16,21 @@ export function closeInfoModal(modal) {
     let loadingAnim = document.querySelector('.loading-anim')
     loadingAnim.classList.remove('active')
 
+    const modalBody = document.querySelector('.info-pop-up__content')
+    const modalFooter = document.querySelector('.info-pop-up__footer')
+    modalBody.innerHTML = ""
+    modalFooter.innerHTML = ""
+
     modal.classList.remove('active')
     overlay.classList.remove('active')
+
+    modalBody.appendChild(loadingAnim)
 }
 
 
 export function loadDescription(description) {
     const modalBody = document.querySelector('.info-pop-up__content')
+    const modalFooter = document.querySelector('.info-pop-up__footer')
 
     let ingredientList = description.ingredients[description.active_lang]
     let optionList = description.options[description.active_lang]
@@ -33,23 +41,37 @@ export function loadDescription(description) {
 
     let ingredientsParagraph = document.createElement('p')
     ingredientsParagraph.innerText = ingredientList.join(', ')
+    ingredientsParagraph.classList.add('info-pop-up__content__ingredients')
 
     let optionsContainer = document.createElement('div')
+    optionsContainer.classList.add('info-pop-up__content__options-container')
     optionList.forEach((option) => {
-        let optionLabel = document.createElement('h2')
+        let optionLabel = document.createElement('div')
+        optionLabel.classList.add('info-pop-up__content__options-container--option')
         optionLabel.innerHTML = option
         optionsContainer.appendChild(optionLabel)
     })
 
+    if (optionsContainer.children.length == 0) {
+        let noOptionsLabel = document.createElement('p')
+        noOptionsLabel.classList.add('info-pop-up__content__options-container--noOptions')
+        noOptionsLabel.innerText = "no other options available for this dish" 
+        optionsContainer.appendChild(noOptionsLabel)
+    }
+
     let labelContainer = document.createElement('div')
+    labelContainer.classList.add('info-pop-up__footer__labels-container')
     labelList.forEach((label) => {
-        let labelImage = document.createElement('img')
-        labelImage.src = `../public/img/labels/${label}.png`
-        labelContainer.appendChild(labelImage)
+        if (label != "") {
+            let labelImage = document.createElement('div')
+            labelImage.classList.add('info-pop-up__footer__labels-container--label')
+            labelImage.style.backgroundImage = `url('../public/img/labels/${label}.png')`
+            labelContainer.appendChild(labelImage)
+        }
     })
 
     modalBody.appendChild(ingredientsParagraph)
     modalBody.appendChild(optionsContainer)
-    modalBody.appendChild(labelContainer)
+    modalFooter.appendChild(labelContainer)
 
 }
