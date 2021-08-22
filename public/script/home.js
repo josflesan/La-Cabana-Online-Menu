6420e787bcdc71b4406c5e6697537a57ea88510a
Joflesan <s2088605@ed.ac.uk>
2021-08-22 22:38:18 +0100

    Image Carousel

--- public/script/home.js
+++ public/script/home.js
@@ -1,4 +1,4 @@
-let languageSelector, closeLanguagePopUpBtn, mainBody, overlay;
+let languageSelector, closeLanguagePopUpBtn, mainBody, overlay, cards, transforms, carousel, lastSwipe;
 
 import { openLangModal, closeLangModal } from './languageModal.js';
 import { parallax } from './parallax.js';
@@ -10,6 +10,32 @@ window.onload = () => {
     languageSelector = document.querySelector('.language-selector');
     closeLanguagePopUpBtn = document.querySelector('.language-pop-up__header--close');
 
+    carousel = document.querySelector('.carousel-gallery')
+    cards = document.getElementsByClassName('carousel-gallery__img')
+
+    lastSwipe = "left"
+    
+    transforms = [
+        {
+            x: 0,
+            z: 0,
+            scale: 1,
+            opacity: 1
+        },
+        {
+            x: '-45%',
+            z: '-50px',
+            scale: 0.8,
+            opacity: 0.8
+        },
+        {
+            x: '45%',
+            z: '-50px',
+            scale: 0.8,
+            opacity: 0.8 
+        }
+    ]
+
     languageSelector.addEventListener('click', () => {
         const modal = document.querySelector('.language-pop-up');
         openLangModal(modal);
@@ -20,6 +46,78 @@ window.onload = () => {
         closeLangModal(modal);
     });
 
+    carousel.addEventListener('click', (e) => {
+        if (e.clientX >= window.innerWidth / 2) {
+            nextLeft()
+        } else {
+            nextRight()
+        }
+    })
+
+    carousel.addEventListener('swiped-left', () => {
+        nextLeft()
+    })
+
+    carousel.addEventListener('swiped-right', () => {
+        nextRight()
+    })
+
+}
+
+function nextTransform(x) {
+    if (x >= cards.length - 1) {
+        x = 0
+    } else {
+        x++
+    }
+    return x
+}
+
+function resetTransform() {
+    transforms = [
+        {
+            x: 0,
+            z: 0,
+            scale: 1,
+            opacity: 1
+        },
+        {
+            x: '-45%',
+            z: '-50px',
+            scale: 0.8,
+            opacity: 0.8
+        },
+        {
+            x: '45%',
+            z: '-50px',
+            scale: 0.8,
+            opacity: 0.8 
+        }
+    ]
+}
+
+function nextLeft() {
+    if (lastSwipe == "right") resetTransform()
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.transform = `translateX(${transforms[nextTransform(i)].x}) translateZ(${transforms[nextTransform(i)].z}) scale(${transforms[nextTransform(i)].scale})`
+        cards[i].style.opacity = transforms[nextTransform(i)].opacity
+    }
+
+    transforms.push(transforms.shift())
+    lastSwipe = "left"
+}
+
+function nextRight() {
+    if (lastSwipe == "left") resetTransform()
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.transform = `translateX(${transforms[nextTransform(i)].x}) translateZ(${transforms[nextTransform(i)].z}) scale(${transforms[nextTransform(i)].scale})`
+        cards[i].style.opacity = transforms[nextTransform(i)].opacity
+    }
+
+    transforms.unshift(transforms.pop())
+    lastSwipe = "right"
 }
 
 window.addEventListener('scroll', function() {
