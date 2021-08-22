9337b238c5a1074cb8938635115d40582198320f
Joflesan <s2088605@ed.ac.uk>
2021-08-22 17:19:54 +0100

    Overlay for Modal

--- public/script/home.js
+++ public/script/home.js
@@ -1,10 +1,11 @@
-let languageSelector, closeLanguagePopUpBtn, mainBody;
+let languageSelector, closeLanguagePopUpBtn, mainBody, overlay;
 
 import { openLangModal, closeLangModal } from './languageModal.js';
 import { parallax } from './parallax.js';
 
 window.onload = () => {
 
+    overlay = document.querySelector('#overlay')
     mainBody = document.querySelector('.home-content');
     languageSelector = document.querySelector('.language-selector');
     closeLanguagePopUpBtn = document.querySelector('.language-pop-up__header--close');
