e5cf6f35aaf6d7e60edd3f16d051f9db9f63daa6
Joflesan <s2088605@ed.ac.uk>
2021-08-22 15:36:55 +0100

    Home Script and Routing

---
+++ public/script/home.js
@@ -0,0 +1,26 @@
+let languageSelector, closeLanguagePopUpBtn, mainBody;
+
+import { openLangModal, closeLangModal } from './languageModal.js';
+import { parallax } from './parallax.js';
+
+window.onload = () => {
+
+    mainBody = document.querySelector('.home-content');
+    languageSelector = document.querySelector('.language-selector');
+    closeLanguagePopUpBtn = document.querySelector('.language-pop-up__header--close');
+
+    languageSelector.addEventListener('click', () => {
+        const modal = document.querySelector('.language-pop-up');
+        openLangModal(modal);
+    });
+
+    closeLanguagePopUpBtn.addEventListener('click', () => {
+        const modal = document.querySelector('.language-pop-up');
+        closeLangModal(modal);
+    });
+
+}
+
+window.addEventListener('scroll', function() {
+    parallax('.logo-area', window.scrollY, 1);
+});
